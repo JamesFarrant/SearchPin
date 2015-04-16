9a1ac1ea27e7387503a8e4e30d12b9edac43986a
James Farrant <jfarrant4@gmail.com>
2015-04-16 18:16:09 +0100

    Fixes JSLint errors

--- background.js
+++ background.js
@@ -1,33 +1,37 @@
 var search_prefix = "";
 
 var actions = {
-  "MISSING" : { tip: "Unknown action" },
-  "SET_PREFIX": { tip: "Set prefix to <match>%TEXT%</match>", act: function(text) { search_prefix = text; save_prefix() }},
-  "CLEAR_PREFIX": { tip: "Clear prefix (%PREFIX%)", act: function(text) { search_prefix = "" }},
-  "PERFORM_SEARCH": { tip: "Search for <dim>%PREFIX%</dim> <match>%TEXT%</match>", act: function(text) {
-        chrome.tabs.getSelected(null, function(tab) {
-                chrome.tabs.update(tab.id, {url: " https://www.google.com/search?q="+ encodeURIComponent(search_prefix) + "%20" + encodeURIComponent(text)});
-            }
-        )
-  }}
+    "MISSING": { tip: "Unknown action" },
+    "SET_PREFIX": { tip: "Set prefix to <match>%TEXT%</match>", act: function (text) {
+        search_prefix = text;
+        save_prefix()
+    }},
+    "CLEAR_PREFIX": { tip: "Clear prefix (%PREFIX%)", act: function (text) {
+        search_prefix = ""
+    }},
+    "PERFORM_SEARCH": { tip: "Search for <dim>%PREFIX%</dim> <match>%TEXT%</match>", act: function (text) {
+        chrome.tabs.getSelected(null, function (tab) {
+            chrome.tabs.update(tab.id, {url: " https://www.google.com/search?q=" + encodeURIComponent(search_prefix) + "%20" + encodeURIComponent(text)});
+        })
+    }}
 };
 
 function resolve_input(text) {
-  var action = "";
-  if(search_prefix == "") {
-    action = "SET_PREFIX";
-  } else if(text == "-") {
-    action = "CLEAR_PREFIX";
-  } else {
-    action = "PERFORM_SEARCH";
-  }
-  return actions[action];
+    var action = "";
+    if (search_prefix === "") {
+        action = "SET_PREFIX";
+    } else if (text === "-") {
+        action = "CLEAR_PREFIX";
+    } else {
+        action = "PERFORM_SEARCH";
+    }
+    return actions[action];
 }
 
-function parse_text(pre_text,text) {
-  // this might have minor ui issues if the user has a prefix containing %TEXT%
-  // probably not worth making a solution as this is very minor
-  return pre_text.replace(/\%PREFIX\%/g,search_prefix).replace(/\%TEXT\%/g,text)
+function parse_text(pre_text, text) {
+    // this might have minor ui issues if the user has a prefix containing %TEXT%
+    // probably not worth making a solution as this is very minor
+    return pre_text.replace(/\%PREFIX\%/g, search_prefix).replace(/\%TEXT\%/g, text)
 }
 
 function save_prefix() {
@@ -39,11 +43,11 @@ function save_prefix() {
 }
 function load_prefix() {
     chrome.storage.sync.get('saved_prefix', function (items) {
-            console.log("items", items);
-            console.log('Prefix set');
+        console.log("items", items);
+        console.log('Prefix set');
 
         search_prefix = items['saved_prefix'];
-        });
+    });
 }
 
 chrome.omnibox.onInputChanged.addListener(
@@ -54,13 +58,14 @@ chrome.omnibox.onInputChanged.addListener(
 
         console.log('inputChanged: ' + text);
         suggest([
-            {content: search_prefix + " " + text, description: "Your search term is " + text}]);
-        });
+            {content: search_prefix + " " + text, description: "Your search term is " + text}
+        ]);
+    });
 
 chrome.omnibox.onInputEntered.addListener(
     function (text) {
         var desired_action = resolve_input(text);
-            desired_action.act(text);
-        });
+        desired_action.act(text);
+    });
 
-load_prefix(); // Saves prefixes across sessions
+load_prefix();

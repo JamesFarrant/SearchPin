4cd5fc1d51c096fadc2947b35a297f6766bc3613
James Farrant <jfarrant4@gmail.com>
2015-04-17 18:04:57 +0100

    Fixes #3 by setting search_prefix to a blank string if it is of type undefined.

--- background.js
+++ background.js
@@ -1,12 +1,13 @@
 var search_prefix = "";
 
 var actions = {
-    "MISSING": { tip: function(text) {
+    "MISSING": { tip: function (text) {
         return "Unknown action";
-    }, act: function(text) {} },
+    }, act: function (text) {
+    } },
     "SET_PREFIX": {
-        tip: function(text) {
-            return "Set prefix to <match>"+text+"</match>"
+        tip: function (text) {
+            return "Set prefix to <match>" + text + "</match>"
         },
         act: function (text) {
             search_prefix = text;
@@ -14,46 +15,46 @@ var actions = {
         }
     },
     "CLEAR_PREFIX": {
-        tip: function(text) {
-            return "Clear prefix ("+search_prefix+")";
+        tip: function (text) {
+            return "Clear prefix (" + search_prefix + ")";
         },
         act: function (text) {
             search_prefix = ""
         }
     },
     "PERFORM_SEARCH": {
-        tip: function(text) {
-            return "Search for <dim>"+search_prefix+"</dim> <match>"+text+"</match>";
+        tip: function (text) {
+            return "Search for <dim>" + search_prefix + "</dim> <match>" + text + "</match>";
         },
         act: function (text) {
-            perform_search(search_prefix+" "+text)
+            perform_search(search_prefix + " " + text)
         }
     },
-    "PERFORM_MULTISEARCH": { 
-        tip: function(text) {
-            if(text.match(/^[ \|]*$/)) {
+    "PERFORM_MULTISEARCH": {
+        tip: function (text) {
+            if (text.match(/^[ \|]*$/)) {
                 // they've entered something like " | "
                 return "Enter multiple searches separated by |";
             }
             // the regex removes any empty final terms
-            var terms = text.replace(/ *\|+ *$/g,"").split("|");
-            for(var t in terms) { 
+            var terms = text.replace(/ *\|+ *$/g, "").split("|");
+            for (var t in terms) {
                 terms[t] = terms[t].trim();
             }
             var str = "";
-            if(terms.length > 1) {
+            if (terms.length > 1) {
                 var last_term = terms.pop();
                 var str = terms.join("</match>, <match>");
-                str += "</match> and <match>"+last_term;
+                str += "</match> and <match>" + last_term;
             } else {
                 str = terms[0];
             }
-            return "Perform a multisearch for <match>"+str+"</match>";
-        }, 
+            return "Perform a multisearch for <match>" + str + "</match>";
+        },
         act: function (text) {
             var terms = text.split("|")
-            for(var i = 0; i < terms.length; i++) {
-                perform_search((search_prefix+" "+terms[i].trim()).trim(),true)
+            for (var i = 0; i < terms.length; i++) {
+                perform_search((search_prefix + " " + terms[i].trim()).trim(), true)
             }
         }
     }
@@ -65,7 +66,7 @@ function resolve_input(text) {
         action = "SET_PREFIX";
     } else if (text === "-") {
         action = "CLEAR_PREFIX";
-    } else if(text.indexOf("|") != -1) {
+    } else if (text.indexOf("|") != -1) {
         action = "PERFORM_MULTISEARCH";
     } else {
         action = "PERFORM_SEARCH";
@@ -75,12 +76,13 @@ function resolve_input(text) {
 
 function perform_search(term, background) { //add background feature
     var url = "https://www.google.com/search?q=" + encodeURIComponent(term);
-    if(background !== true) {
+    if (background !== true) {
         chrome.tabs.getSelected(null, function (tab) {
             chrome.tabs.update(tab.id, {url: url});
         })
     } else {
-        chrome.tabs.create({url:url,selected:false},function(tab){});
+        chrome.tabs.create({url: url, selected: false}, function (tab) {
+        });
     }
 }
 
@@ -96,32 +98,35 @@ function load_prefix() {
     chrome.storage.sync.get('saved_prefix', function (items) {
         console.log("items", items);
         console.log('Prefix set');
+        if (search_prefix == undefined) {
+            search_prefix = "";
+        }
 
         search_prefix = items['saved_prefix'];
     });
 }
 
-function give_suggestions(text, suggest) { 
-    if((search_prefix+text).trim().length > 0) {
+function give_suggestions(text, suggest) {
+    if ((search_prefix + text).trim().length > 0) {
         var xhr = new XMLHttpRequest();
 
-        xhr.open("GET", "http://suggestqueries.google.com/complete/search?client=chrome&q="+encodeURIComponent(search_prefix+" "+text), false);
+        xhr.open("GET", "http://suggestqueries.google.com/complete/search?client=chrome&q=" + encodeURIComponent(search_prefix + " " + text), false);
         xhr.send();
 
         var result = JSON.parse(xhr.responseText);
 
-        if(result[1].length > 0) {
-            for(var i = 0; i < 5 && i < result[1].length; i++) {
+        if (result[1].length > 0) {
+            for (var i = 0; i < 5 && i < result[1].length; i++) {
                 var r = result[1][i];
                 console.log(r);
-                if(search_prefix==="") {
+                if (search_prefix === "") {
                     suggest([
-                        {content: r, description: "<match>"+r+"</match>"}
+                        {content: r, description: "<match>" + r + "</match>"}
                     ]);
-                } else if(r.startsWith(search_prefix)) {
-                    var search = r.substr(search_prefix.length+1)
+                } else if (r.startsWith(search_prefix)) {
+                    var search = r.substr(search_prefix.length + 1)
                     suggest([
-                        {content: search, description: "<dim>"+search_prefix+"</dim> <match>"+search+"</match>"}
+                        {content: search, description: "<dim>" + search_prefix + "</dim> <match>" + search + "</match>"}
                     ]);
                 }
             }
@@ -135,7 +140,7 @@ chrome.omnibox.onInputChanged.addListener(
         var tip_text = desired_action.tip(text);
         chrome.omnibox.setDefaultSuggestion({description: tip_text});
 
-        give_suggestions(text,suggest);
+        give_suggestions(text, suggest);
     }
 );
 
